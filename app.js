a564ec0cdd16b096452abf21561af0d3acf93bf7
mukhammadrizo <pulatovvmukhammadrizo@gmail.com>
2024-02-12 02:08:24 +0500

    work with javascript

---
+++ app.js
@@ -0,0 +1,36 @@
+let familya = 'Pulatovv';
+let ism = 'Muhammadrizo';
+let yosh = '18';
+console.log( familya,ism,yosh );
+
+yosh = 19;
+console.log(yosh);
+
+const familiya = 'Pulatov';
+console.log(familiya);
+
+var joy = 'Fergana';
+joy = 'Qoqon shaxar';
+console.log(joy);
+
+let firstName = 'Muhammadrizo';
+let lastName = 'Pulatov';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+console.log(fullName.length);
+
+console.log(fullName.toUpperCase());
+
+let result = fullName.toLowerCase();
+console.log(result);
+
+
+
+
+
+
+
+
+
